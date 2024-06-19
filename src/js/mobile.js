73a21f70f961c8634c88248a6cc52cb82e1991a2
Nufexo <antgranada@gmail.com>
2024-06-19 18:24:47 +0200

    add mobile nav

---
+++ src/js/mobile.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById('mobile__navbar')
+export function openMobile() {
+    const openMobile = document.getElementById('openMobile')
+    openMobile.addEventListener('click', () => {
+      menu.style.display = 'flex'
+    })}
+
+export function closeMobile() {
+    const closeMobile = document.getElementById('closeMobile')
+    closeMobile.addEventListener('click', () => {
+      menu.style.display = 'none'
+    })}
