2cfa61c1f1c04068cf0c50403939a4f5a12bbf9e
Nufexo <antgranada@gmail.com>
2024-07-10 13:27:15 +0200

    fix persistant mobile menu for future translation

---
+++ src/js/mobile.js
@@ -0,0 +1,54 @@
+function openMobile (menu,openButton){
+  openButton.addEventListener('click', () => {
+    console.log('button clicked')
+    if (menu.classList.contains('show')) {
+      menu.style.animationName = 'hide'
+      setTimeout(() => {
+        menu.classList.remove('show')
+      }, 300)
+    } else {
+      menu.style.animationName = 'show'
+      menu.classList.add('show')
+    }
+  })
+}
+function closeMobile (menu,openButton){
+  window.addEventListener('click', function (e) {
+    const { target } = e
+    if (
+      !menu.contains(target) &&
+      !openButton.contains(target) &&
+      menu.classList.contains('show')
+    ) {
+      menu.style.animationName = 'hide'
+      setTimeout(() => {
+        menu.classList.remove('show')
+      }, 500)
+    }
+  })
+}
+
+document.addEventListener('astro:page-load',()=>{
+
+const menu = document.getElementById('mobile__navbar')
+const openButton = document.getElementById('openMobile')
+  openMobile(menu,openButton)
+  closeMobile(menu,openButton)
+})
+
+document.addEventListener('astro:after-swap',()=>{
+  const menu = document.getElementById('mobile__navbar')
+  if (menu.classList.contains('show')) {
+    menu.style.animationName = 'hide'
+    
+  } else {
+    menu.style.animationName = 'show'
+    menu.classList.add('show')
+  }
+
+})
+
+
+
+
+
