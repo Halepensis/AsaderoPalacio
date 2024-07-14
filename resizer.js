d23dc207220f99366c74612e9c27dcbe310bc1e9
Nufexo <antgranada@gmail.com>
2024-07-14 18:09:40 +0200

    Change jpg sizes and modify resizer to be more precise

--- resizer.js
+++ resizer.js
@@ -2,29 +2,35 @@ import imageSize from 'image-size'
 import * as fs from "fs"
 import * as path from "path"
 
-const directoryPath = path.join("public","images","galeria/")
-const publicPath = path.join("images","galeria/")
+const directoryPath = path.join("public","images","galeria\\")
+const publicPath = path.join("\\images","galeria\\")
 let imagesSizes = []
 fs.readdir(directoryPath, function (err, files) {
   if (err) {
       return console.log('Unable to scan directory: ' + err);
   } 
-  files.forEach(function (file,index) {
+  let counter = 0
+  files.forEach(function (file) {
+    if(file.split('.')[1]==='jpg'){
+      console.log(counter)
+      console.log(file)
       const data = imageSize(directoryPath+file)
       const width = data.width
       const height = data.height
       const filename = file.split(".")[0]
-      imagesSizes[index]={
+      imagesSizes[counter]={
         jpgSrc:publicPath+file,
-        webpSrc:directoryPath+filename+".webp",
+        webpSrc:publicPath+filename+".webp",
         width:width,
         height:height,
         description:`Una imagen de ${filename}`
-        }
-        
-      }) 
-      fs.writeFile("./src/data/galeria.json",JSON.stringify(imagesSizes),(err)=>{
-        if(err) return console.log(err)
+      }
+      counter++
+    }
+    
+  }) 
+  fs.writeFile("./src/data/galeria.json",JSON.stringify(imagesSizes),(err)=>{
+      if(err) return console.log(err)
         console.log("The file was saved")
       })
   });
